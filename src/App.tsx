f7ee1c84e2843cace3172b8fc57797379ac4e986
dvankevich <dvankevich@gmail.com>
2024-12-09 21:11:17 +0200

    Using a Custom Home Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import { Admin, Resource, ShowGuesser } from "react-admin";
-import { Layout } from "./Layout";
+// import { Layout } from "./Layout";
 import { dataProvider } from "./dataProvider";
 import { PostList, PostEdit, PostCreate } from "./posts";
 import { UserList } from "./users";
 import PostIcon from "@mui/icons-material/Book";
 import UserIcon from "@mui/icons-material/Group";
+import { Dashboard } from "./Dashboard";
 
 export const App = () => (
   // <Admin layout={Layout} dataProvider={dataProvider}>
@@ -16,7 +17,7 @@ export const App = () => (
   //   />
   //   <Resource name="users" list={UserList} show={ShowGuesser} />
   // </Admin>
-  <Admin dataProvider={dataProvider}>
+  <Admin dataProvider={dataProvider} dashboard={Dashboard}>
     <Resource
       name="posts"
       list={PostList}
