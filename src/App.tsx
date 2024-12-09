aed0c15c7b38c952bbe261a330b332c76ef9303a
dvankevich <dvankevich@gmail.com>
2024-12-09 18:19:57 +0200

    Handling Relationships

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Admin, Resource } from "react-admin";
 import { Layout } from "./Layout";
 import { dataProvider } from "./dataProvider";
+import { PostList } from "./posts";
 import { UserList } from "./users";
 
 export const App = () => (
   <Admin layout={Layout} dataProvider={dataProvider}>
+    <Resource name="posts" list={PostList} />
     <Resource name="users" list={UserList} />
   </Admin>
 );
---
+++ src/posts.tsx
@@ -0,0 +1,19 @@
+import {
+  List,
+  Datagrid,
+  TextField,
+  ReferenceField,
+  EditButton,
+} from "react-admin";
+
+export const PostList = () => (
+  <List>
+    <Datagrid rowClick={false}>
+      <TextField source="id" />
+      <ReferenceField source="userId" reference="users" />
+      <TextField source="title" />
+      {/* <TextField source="body" /> */}
+      <EditButton />
+    </Datagrid>
+  </List>
+);
