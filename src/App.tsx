a4f185824449ce9eecb5b37e02d70878f485e3c7
dima <dvankevich@gmail.com>
2024-12-09 11:33:57 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { Admin, Resource } from "react-admin";
+import { Layout } from "./Layout";
+import { dataProvider } from "./dataProvider";
+import { UserList } from "./users";
+
+export const App = () => (
+  <Admin layout={Layout} dataProvider={dataProvider}>
+    <Resource name="users" list={UserList} />
+  </Admin>
+);
---
+++ src/users.tsx
@@ -0,0 +1,27 @@
+// import { Datagrid, EmailField, List, TextField } from 'react-admin';
+
+// export const UserList = () => (
+// 	<List>
+// 		<Datagrid>
+// 			<TextField source="id" />
+// 			<TextField source="name" />
+// 			<TextField source="username" />
+// 			<EmailField source="email" />
+// 			<TextField source="address.street" />
+// 			<TextField source="phone" />
+// 			<TextField source="website" />
+// 			<TextField source="company.name" />
+// 		</Datagrid>
+// 	</List>
+// );
+import { List, SimpleList } from "react-admin";
+
+export const UserList = () => (
+    <List>
+        <SimpleList
+          primaryText={(record) => record.name}
+          secondaryText={(record) => record.username}
+          tertiaryText={(record) => record.email}
+        />
+    </List>
+);
