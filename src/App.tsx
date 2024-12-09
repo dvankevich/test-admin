031e2176b2ba916942aacae9793d01e56364188f
dvankevich <dvankevich@gmail.com>
2024-12-09 21:05:58 +0200

    Customizing the Menu Icons

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,27 @@ import { Layout } from "./Layout";
 import { dataProvider } from "./dataProvider";
 import { PostList, PostEdit, PostCreate } from "./posts";
 import { UserList } from "./users";
+import PostIcon from "@mui/icons-material/Book";
+import UserIcon from "@mui/icons-material/Group";
 
 export const App = () => (
-  <Admin layout={Layout} dataProvider={dataProvider}>
+  // <Admin layout={Layout} dataProvider={dataProvider}>
+  //   <Resource
+  //     name="posts"
+  //     list={PostList}
+  //     edit={PostEdit}
+  //     create={PostCreate}
+  //   />
+  //   <Resource name="users" list={UserList} show={ShowGuesser} />
+  // </Admin>
+  <Admin dataProvider={dataProvider}>
     <Resource
       name="posts"
       list={PostList}
       edit={PostEdit}
       create={PostCreate}
+      icon={PostIcon}
     />
-    <Resource name="users" list={UserList} show={ShowGuesser} />
+    <Resource name="users" list={UserList} show={ShowGuesser} icon={UserIcon} />
   </Admin>
 );
