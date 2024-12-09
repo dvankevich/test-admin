8bbb53de1dceb3aa73db8c436e63f100d660c5fd
dvankevich <dvankevich@gmail.com>
2024-12-09 17:44:24 +0200

    Using Field Types

--- src/users.tsx
+++ src/users.tsx
@@ -1,3 +1,35 @@
+//Step 3
+// https://marmelab.com/react-admin/Tutorial.html#composing-components
+
+import { useMediaQuery, Theme } from "@mui/material";
+import { List, SimpleList, Datagrid, TextField, EmailField, UrlField } from "react-admin";
+
+export const UserList = () => {
+    const isSmall = useMediaQuery<Theme>((theme) => theme.breakpoints.down("sm"));
+    return (
+        <List>
+            {isSmall ? (
+                <SimpleList
+                    primaryText={(record) => record.name}
+                    secondaryText={(record) => record.username}
+                    tertiaryText={(record) => record.email}
+                />
+            ) : (
+                <Datagrid>
+                    <TextField source="id" />
+                    <TextField source="name" />
+                    {/* <TextField source="username" /> */}
+                    <EmailField source="email" />
+                    {/* <TextField source="address.street" /> */}
+                    <TextField source="phone" />
+                    <UrlField source="website" />
+                    <TextField source="company.name" />
+                </Datagrid>
+            )}
+        </List>
+    );
+};
+// Step 1
 // import { Datagrid, EmailField, List, TextField } from 'react-admin';
 
 // export const UserList = () => (
@@ -14,14 +46,16 @@
 // 		</Datagrid>
 // 	</List>
 // );
-import { List, SimpleList } from "react-admin";
 
-export const UserList = () => (
-    <List>
-        <SimpleList
-          primaryText={(record) => record.name}
-          secondaryText={(record) => record.username}
-          tertiaryText={(record) => record.email}
-        />
-    </List>
-);
+// Step 2
+//import { List, SimpleList } from "react-admin";
+
+// export const UserList = () => (
+//     <List>
+//         <SimpleList
+//           primaryText={(record) => record.name}
+//           secondaryText={(record) => record.username}
+//           tertiaryText={(record) => record.email}
+//         />
+//     </List>
+// );
